bfcaaac0930a701d5c40476713e57a6bd06fe0cb
Elxan <34681883+LordKratos26@users.noreply.github.com>
2019-04-04 20:45:12 +0400

    Add files via upload

---
+++ Cbar/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Cbar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dgwvalyuta = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwvalyuta)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(227, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Tarixe gore goster";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dgwvalyuta
+            // 
+            this.dgwvalyuta.AllowUserToAddRows = false;
+            this.dgwvalyuta.AllowUserToDeleteRows = false;
+            this.dgwvalyuta.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwvalyuta.Location = new System.Drawing.Point(12, 51);
+            this.dgwvalyuta.Name = "dgwvalyuta";
+            this.dgwvalyuta.ReadOnly = true;
+            this.dgwvalyuta.Size = new System.Drawing.Size(331, 387);
+            this.dgwvalyuta.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 450);
+            this.Controls.Add(this.dgwvalyuta);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
+            this.Text = "Valyuta";
+            ((System.ComponentModel.ISupportInitialize)(this.dgwvalyuta)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dgwvalyuta;
+    }
+}
+
---
+++ Cbar/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+
+namespace Cbar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            getcurrencybydate(dateTimePicker1.Value.ToString("dd.MM.yyyy"));
+
+
+        }
+
+        public void getcurrencybydate (string p_date)
+        {
+             
+            Uri uri = new Uri($"https://www.cbar.az/currencies/{p_date}.xml");
+            WebRequest webRequest = WebRequest.Create(uri);
+            webRequest.Proxy = null;
+            WebResponse webResponse = webRequest.GetResponse();
+            XmlDocument doc1 = new XmlDocument();
+            doc1.Load(webResponse.GetResponseStream());
+            DataSet dt = new DataSet();
+            XmlNodeReader xnr = new XmlNodeReader(doc1);
+            dt.ReadXml(xnr);
+            dt.Tables[1].Rows[0].Delete();
+            dgwvalyuta.DataSource = dt.Tables[2];
+        }
+     
+
+
+    }
+}
